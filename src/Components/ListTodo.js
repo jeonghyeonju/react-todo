f174734805bca6dfc44ad1df92aa4a3a004021b5
JeongHyeonJu <hyeon6921@gmail.com>
2016-12-08 23:52:30 +0900

    수정 input show, hide

--- src/Components/ListTodo.js
+++ src/Components/ListTodo.js
@@ -8,6 +8,7 @@ import IconButton from 'material-ui/IconButton';
 import {grey400} from 'material-ui/styles/colors';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 import IconMenu from 'material-ui/IconMenu';
+import TextField from 'material-ui/TextField';
 
 const iconButtonElement = (
   <IconButton touch={true} >
@@ -16,28 +17,51 @@ const iconButtonElement = (
 );
 
 export default class ListTodo extends Component {
+  state = {
+    todovalue : '',
+    edit: false
+  }
   rightIconMenu(it) {
     const {onDelete} = this.props;
     return (
       <IconMenu iconButtonElement={iconButtonElement}>
-        <MenuItem>Edit</MenuItem>
+        <MenuItem onClick={this._onEdit.bind(this)}>Edit</MenuItem>
         <MenuItem onClick={() => onDelete(it)}>Delete</MenuItem>
       </IconMenu>
     )
   }
   render() {
     const {onDelete, onToggle} = this.props;
+    const {todovalue} = this.state
     return (
       <List>
         {(() => this.props.list.map((it, i) => (
-          <ListItem
+          <ListItem id={i}
             key={i}
             leftCheckbox={<Checkbox checked={it.isDone} onCheck={() => onToggle(i)} />}
             rightIconButton={ this.rightIconMenu(it) }>
-            <span className={ it.isDone ? 'contents-is-done' : 'contents-default' }>{it.contents}</span>
+            {
+              !this.state.edit ? (<span
+                className={ it.isDone ? 'contents-is-done' : 'contents-default' }>{it.contents}</span>) : null
+            }{
+              this.state.edit ? ( <TextField
+              style={{width: '100%'}}
+              hintText={it.contents}
+              floatingLabelText="edit your todo item."
+              type="text" value={this.state.todovalue} onChange={this._onChange.bind(this)} />) : null
+
+             }
           </ListItem>
         )))()}
       </List>
     )
+  }
+  _onEdit(){
+    console.log('1')
+    this.setState({edit: true})
+  }
+  _onChange(event){
+    this.setState({todovalue: event.target.value})
+    event.preventDefault();
   }
 }
